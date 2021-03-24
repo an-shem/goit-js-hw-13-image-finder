5030a74ec9d29a6ea2cfe443dbb8f5e133558258
Andrey Shemet <an-shem@ukr.net>
2021-03-24 07:25:45 +0200

    Создал переменные для API.

---
+++ src/js/apiService.js
@@ -0,0 +1,6 @@
+const MY_KEY = '20832523-2bf34066ca306390d6c2fd3bb';
+const BASE_URL = `https://pixabay.com/api/`;
+const URL = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${MY_KEY}`;
+
+const searchQuery = '';
+const page = 1;
